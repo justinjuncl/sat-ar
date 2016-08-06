62bb6f8d02fff98c9fd022b3d48b01d2ddb4d8aa
Justin Jun <justinjun.cl@gmail.com>
2016-08-07 01:51:45 +0900

    realtime tracking iss

--- public/main.js
+++ public/main.js
@@ -28,20 +28,11 @@ scene.add( bottomHemiEdge );
 
 // ---------------------------------------------------------------
 
-
-
 var issRecord = new SatelliteRecord([
 	"0 ISS (ZARYA)",
 	"1 25544U 98067A   16218.77412066  .00002369  00000-0  41840-4 0  9992",
 	"2 25544  51.6438 170.6068 0002061 103.6851 332.7169 15.54924509 12705"
 ]);
-
-var currentTime = new Date();
-
-var positionAndVelocity = issRecord.getPositionAndVelocityAtTime( currentTime );
-
-var positionEci = positionAndVelocity.position,
-    velocityEci = positionAndVelocity.velocity;
 
 var observerGd = {
     longitude: 127.045153 * satellite.deg2rad,
@@ -49,45 +40,25 @@ var observerGd = {
     height: 0.370
 };
 
-var gmst = satellite.gstimeFromTime( currentTime );
+    // // Geodetic coords are accessed via `longitude`, `latitude`, `height`.
+    // var longitude = positionGd.longitude,
+    //     latitude  = positionGd.latitude,
+    //     height    = positionGd.height;
 
-// You can get ECF, Geodetic, Look Angles, and Doppler Factor.
-var positionEcf   = satellite.eciToEcf(positionEci, gmst),
-    observerEcf   = satellite.geodeticToEcf(observerGd),
-    positionGd    = satellite.eciToGeodetic(positionEci, gmst),
-    lookAngles    = satellite.ecfToLookAngles(observerGd, positionEcf);
+    // //  Convert the RADIANS to DEGREES for pretty printing (appends "N", "S", "E", "W". etc).
+    // var longitudeStr = satellite.degreesLong(longitude),
+    //     latitudeStr  = satellite.degreesLat(latitude);
 
-
-    // Look Angles may be accessed by `azimuth`, `elevation`, `range_sat` properties.
-    var azimuth   = lookAngles.azimuth,
-        elevation = lookAngles.elevation,
-        rangeSat  = lookAngles.rangeSat/80;
-
-    //azimuth to cartesian
-    var satelliteX = rangeSat * Math.cos(elevation) * Math.sin(azimuth),
-        satelliteY = rangeSat * Math.cos(elevation) * Math.cos(azimuth),
-        satelliteZ = rangeSat * Math.sin(elevation);
-
-    // Geodetic coords are accessed via `longitude`, `latitude`, `height`.
-    var longitude = positionGd.longitude,
-        latitude  = positionGd.latitude,
-        height    = positionGd.height;
-
-    //  Convert the RADIANS to DEGREES for pretty printing (appends "N", "S", "E", "W". etc).
-    var longitudeStr = satellite.degreesLong(longitude),
-        latitudeStr  = satellite.degreesLat(latitude);
 
 var issSphere = new THREE.SphereGeometry( 3, 15, 15);
 var issMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
 var iss = new THREE.Mesh( issSphere, issMaterial );
 
-iss.position.x = satelliteX;
-iss.position.y = satelliteY;
-iss.position.z = satelliteZ;
-
 scene.add(iss);
 console.log("pos" ,latitudeStr,longitudeStr);
 
+
+var currentTime, positionAndVelocity, positionEci, velocityEci, gmst, positionEci, observerEcf, positionGd, lookAngles, azimuth, elevation, rangeSat, satelliteX, satelliteY, satelliteZ;
 
 // ---------------------------------------------------------------
 
@@ -95,6 +66,39 @@ function render () {
 	requestAnimationFrame( render );
 
 	gyroControls.update();
+
+
+    currentTime = new Date();
+
+    positionAndVelocity = issRecord.getPositionAndVelocityAtTime( currentTime );
+
+    positionEci = positionAndVelocity.position,
+    velocityEci = positionAndVelocity.velocity;
+
+
+    gmst = satellite.gstimeFromTime( currentTime );
+
+// You can get ECF, Geodetic, Look Angles, and Doppler Factor.
+    positionEcf   = satellite.eciToEcf(positionEci, gmst),
+    observerEcf   = satellite.geodeticToEcf(observerGd),
+    positionGd    = satellite.eciToGeodetic(positionEci, gmst),
+    lookAngles    = satellite.ecfToLookAngles(observerGd, positionEcf);
+
+
+    // Look Angles may be accessed by `azimuth`, `elevation`, `range_sat` properties.
+        azimuth   = lookAngles.azimuth,
+        elevation = lookAngles.elevation,
+        rangeSat  = lookAngles.rangeSat/80;
+
+    //azimuth to cartesian
+        satelliteX = rangeSat * Math.cos(elevation) * Math.sin(azimuth),
+        satelliteY = rangeSat * Math.cos(elevation) * Math.cos(azimuth),
+        satelliteZ = rangeSat * Math.sin(elevation);
+
+iss.position.x = satelliteX;
+iss.position.y = satelliteY;
+iss.position.z = satelliteZ;
+
 
 	renderer.render(scene, camera);
 };
